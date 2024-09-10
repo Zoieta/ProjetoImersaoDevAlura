7f363c6be4b9532bd6d278017a51dfa1d407530e
Izatech <156534461+Zoieta@users.noreply.github.com>
2024-09-10 10:06:25 +0100

    Update app.js

--- imersaoDev/app.js
+++ imersaoDev/app.js
@@ -2,13 +2,32 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Verifica se o campo de pesquisa está vazio
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Você precisa digitar o nome de um atleta ou esporte</p>";
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags ? dado.tags.toLowerCase() : ""; // Verifica se o campo tags existe
+
+        // Verifica se o campoPesquisa está no título, descrição ou tags
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemento de resultado
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -16,12 +35,15 @@ function pesquisar() {
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
-        `;
+            `;
+        }
+    }
+
+    // Se nenhum resultado foi encontrado, exibe mensagem
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>";
     }
 
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
-
-
-
